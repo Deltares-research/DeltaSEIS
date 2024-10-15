be90448c89a70af5a053783d130f710cbadbdb26
Roeland <roeland.nieboer@deltares.nl>
2024-10-15 12:43:11 +0200

    update init file

--- deltaseis/__init__.py
+++ deltaseis/__init__.py
@@ -1,5 +1,6 @@
 from deltaseis.readers.reader_seismic_semd import read_semd
-from deltaseis.processing.preprocessing import resample, deltaseis_to_obspy, obspy_to_deltaseis, masw
+from deltaseis.processing.preprocessing import resample, deltaseis_to_obspy, obspy_to_deltaseis
+from deltaseis.surfacewaves import masw
 from deltaseis.export.export_seismic_sg2 import export_sg2, export_sgy
 from deltaseis.tools.segy_editor import Segy_edit
 from deltaseis.tools.segy_editor_numba import Segy_edit_numba
