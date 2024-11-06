92c5e464d880f0dd752feae8003d6c45fb5ebc1b
Roeland <roeland.nieboer@deltares.nl>
2024-11-06 20:56:37 +0100

    added functions to base seismic and segy_editor

--- tutorials/waal_seismiek.py
+++ tutorials/waal_seismiek.py
@@ -13,7 +13,6 @@ bathy_path = Path(r"D:\Projects\PES waal\qgis\20210623PES waal S2_Area 3_RD_NAP_
 
 edit = Segy_edit(segy_file)
 edit.set_crs(28992)
-data = np.array(edit.trace_data).T
 
 manual_recording_delay = 4 #ms
 
@@ -34,10 +33,7 @@ seis.agc_gain()
 ## kirchhoff migration would be cool here
 ## multiple suppression would be cool here
 
-## end data processing
 
-edit.gain_type = 2 #t2 gain
-edit.trace_data = seis.data.T.astype('int32')
 
 ## import bathymetry and verical correction with seabed pick
 bathy = xr.open_dataset(bathy_path)
@@ -55,7 +51,6 @@ edit.write(segy_file.parent / 'processed.sgy')
 NEXT STEPS:
 - find the bottleneck for processing (bandpass filter is probably the slowest and not really needed)
 - seabedpick and and correct with bathy (using a netcdf file, meaning the get_bathy function should be updated)
-- get
 - trace selection based on 4 crosspoints (set manually by plotting the cross section with the qgis exports of the tracks)
 - find closest point, calculate distance of all points from THAT specific point. Select traces based on distance < x e.g 50 on both sides
 - conversion of all the raw data to make a clean repo would be an excellent idea (raw and processed)
